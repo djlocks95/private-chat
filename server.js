da0230b02ecf86226aaf523b1287d666ec41d93e
djlocks95 <130712070+djlocks95@users.noreply.github.com>
2026-07-12 21:15:28 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,56 +1,189 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static("public"));
 const WebSocket = require("ws");
 
-const server = new WebSocket.Server({
-    port: 3000
+const httpServer = app.listen(3000, () => {
+
+    console.log("Website running");
+
 });
 
+
+const server = new WebSocket.Server({
+    server:httpServer
+});
+
+
 let rooms = {};
+
+let admins = [];
+
 
 console.log("Server running on port 3000");
 
 
+
 server.on("connection", socket => {
 
+
     socket.on("message", data => {
+
 
         let msg = JSON.parse(data);
 
 
-        // When someone joins a room
-        if (msg.type === "join") {
+
+        // USER JOINS ROOM
+        if(msg.type === "join"){
+
+
+            socket.username = msg.username;
 
             socket.room = msg.room;
 
 
-            if (!rooms[msg.room]) {
+
+            if(!rooms[msg.room]){
+
                 rooms[msg.room] = [];
+
             }
+
 
 
             rooms[msg.room].push(socket);
 
 
-            console.log("User joined room:", msg.room);
+
+            console.log(
+                socket.username +
+                " joined " +
+                msg.room
+            );
+
+
+
+            sendAdminUpdate();
 
         }
 
 
-        // Send messages to others in the same room
-        if (msg.type === "message") {
 
-            if (rooms[socket.room]) {
 
-                rooms[socket.room].forEach(client => {
 
-                    if (client !== socket) {
+        // NORMAL CHAT MESSAGE
+        if(msg.type === "message"){
 
-                        client.send(JSON.stringify(msg));
 
-                    }
+            if(rooms[socket.room]){
+
+
+                rooms[socket.room].forEach(client =>{
+
+
+                    client.send(JSON.stringify({
+
+                        type:"message",
+
+                        user:socket.username,
+
+                        text:msg.text
+
+                    }));
+
 
                 });
 
+
             }
+
+
+        }
+
+
+
+
+
+        // ADMIN LOGIN
+        if(msg.type === "admin"){
+
+
+            if(msg.password === "ADMIN123"){
+
+
+                socket.isAdmin = true;
+
+
+                admins.push(socket);
+
+
+
+                socket.send(JSON.stringify({
+
+                    type:"adminLogin",
+
+                    rooms:Object.keys(rooms)
+
+                }));
+
+
+
+                console.log("Admin connected");
+
+
+            }
+
+
+        }
+
+
+
+
+
+        // ADMIN CLOSE ROOM
+        if(msg.type === "closeRoom"){
+
+
+            let room = msg.room;
+
+
+
+            if(rooms[room]){
+
+
+                rooms[room].forEach(client =>{
+
+
+                    client.send(JSON.stringify({
+
+                        type:"closed",
+
+                        message:
+                        "Room closed by admin"
+
+
+                    }));
+
+
+                    client.close();
+
+
+                });
+
+
+
+                delete rooms[room];
+
+
+
+                sendAdminUpdate();
+
+            }
+
 
         }
 
@@ -58,18 +191,61 @@ server.on("connection", socket => {
     });
 
 
-    socket.on("close", () => {
 
-        if (socket.room && rooms[socket.room]) {
+
+
+    socket.on("close",()=>{
+
+
+        if(socket.room && rooms[socket.room]){
+
 
             rooms[socket.room] =
             rooms[socket.room].filter(
-                client => client !== socket
+                c=>c!==socket
             );
 
+
+            if(rooms[socket.room].length===0){
+
+                delete rooms[socket.room];
+
+            }
+
+
+            sendAdminUpdate();
+
         }
+
+
 
     });
 
 
+
 });
+
+
+
+
+
+function sendAdminUpdate(){
+
+
+    admins.forEach(admin=>{
+
+
+        admin.send(JSON.stringify({
+
+            type:"rooms",
+
+            rooms:Object.keys(rooms)
+
+
+        }));
+
+
+    });
+
+
+}
